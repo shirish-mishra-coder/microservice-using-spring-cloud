c8bff64ca71c2fe2af7ea7090ceb5e38c713d2b6
Shirish Mishra <shirishmishra.connect@gmail.com>
2022-04-07 18:22:42 +0530

    commit limits-service project

---
+++ limits-service/src/main/java/com/prabha/techno/controller/LimitsController.java
@@ -0,0 +1,22 @@
+package com.prabha.techno.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.prabha.techno.bean.Limits;
+import com.prabha.techno.configuration.Configuration;
+
+@RestController
+public class LimitsController {
+	
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping("/limits")
+	public Limits retrieveLimits() {
+		//return new Limits(1,1000);
+		
+		return new Limits(configuration.getMinimum(), configuration.getMaximum());
+	}
+}
